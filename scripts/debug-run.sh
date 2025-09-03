1a02d88ca93205223fba9f0e3737c3fc4043dbc1
maitrey-gramo <111958072+maitrey-gramo@users.noreply.github.com>
2025-09-03 14:33:58 -0400

    Delete scripts directory

--- scripts/debug-run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python src/cogelot/entrypoints/train.py debug=nccl hardware=eidf_4gpu experiment=01_their_vima 2>&1 | tee current-run.log
--- scripts/run-sweep-4.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-wandb agent pyop/CoGeLoT/fkteqx1r
